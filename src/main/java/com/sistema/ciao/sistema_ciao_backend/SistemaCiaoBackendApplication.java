bb20f7952aad32f794421d5c64c2f282c87be790
Krudarks <agustinmigueliop@gmail.com>
2024-06-11 23:45:07 -0600

    ciao-backend

---
+++ src/main/java/com/sistema/ciao/sistema_ciao_backend/SistemaCiaoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.ciao.sistema_ciao_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCiaoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCiaoBackendApplication.class, args);
+	}
+
+}
