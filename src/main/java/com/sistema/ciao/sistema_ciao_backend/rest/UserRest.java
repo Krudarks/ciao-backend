bf2c5ccc4c8c5bd2995e4a94b1cc331fe0eb4246
Krudarks <65472660+Krudarks@users.noreply.github.com>
2024-06-17 20:38:25 -0600

    Update UserRest.java
    postmapping

--- src/main/java/com/sistema/ciao/sistema_ciao_backend/rest/UserRest.java
+++ src/main/java/com/sistema/ciao/sistema_ciao_backend/rest/UserRest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,7 @@ public class UserRest {
     }
 
     //Metodo para crear un usuario
+    @PostMapping
     public ResponseEntity<UserEntity>createuser(@RequestBody UserEntity user) {
         UserEntity savedUser = userService.saveUser(user);
         return ResponseEntity.ok(savedUser);     
